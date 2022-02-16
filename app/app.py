03329ceb1dc2312d99578acc9fb85ce797fe6995
Caio Carvalho <21188280+lowercase00@users.noreply.github.com>
2022-02-16 20:39:27 -0300

    Update

--- app/app.py
+++ app/app.py
@@ -2,8 +2,15 @@ from flask import Flask
 from dynaconf import FlaskDynaconf
 
 app = Flask(__name__)
-FlaskDynaconf(app, envvar_prefix='PEANUT')
+FlaskDynaconf(
+    app,
+    envvar_prefix='PEANUT',
+    settings_files=[
+        "settings.toml",
+        ".secrets.toml"
+    ]
+)
 
 @app.route('/')
 def index():
-    return app.config['SQLALCHEMY_DATABASE_URI']
+    return app.config['CUSTOM_VAR']
